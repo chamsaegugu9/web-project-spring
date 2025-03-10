fc03e76542e5932ec8aed5d8e7133e10458e5056
chamsaegugu9 <rkdehdtjd123@naver.com>
2025-03-10 22:03:08 +0900

    Hello

--- demo/demo/src/main/java/com/example/demo/entity/DemoEntity.java
+++ demo/demo/src/main/java/com/example/demo/entity/DemoEntity.java
@@ -1,5 +1,40 @@
 package com.example.demo.entity;
 
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
 public class DemoEntity {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO) 
+    private String userCode;
+
+    @Column(nullable = false, unique = true)
+    private String id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @PrePersist
+    public void prePersist(){
+        if(userCode==null){
+            userCode = UUID.randomUUID().toString();
+        }
+    }
 }
--- demo/demo/src/main/java/com/example/demo/repository/DemoRepository.java
+++ demo/demo/src/main/java/com/example/demo/repository/DemoRepository.java
@@ -1,8 +1,14 @@
 package com.example.demo.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.example.demo.entity.DemoEntity;
+
 @Repository
-public interface DemoRepository extends JpaRepository{   
+public interface DemoRepository extends JpaRepository<DemoEntity,String>{
+    List<DemoEntity> findAllByName(String name);
+    DemoEntity findByName(String name);
 }
